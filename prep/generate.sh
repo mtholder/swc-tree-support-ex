fd4228e9ea7879297e785ce8923e7e289063ed67
Mark T. Holder <mtholder@gmail.com>
2013-08-22 13:05:54 -0500

    shell script and example intermediate output from seq-gen

--- prep/generate.sh
+++ prep/generate.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 set -x
-#seq-gen -mHKY -n1000 -or tree.txt > simdata.txt || exit
-python phylip2json.py simdata.txt > simdata.json || exit
-echo 'Succeeded'
+
+seq-gen -mHKY -n1000 -or tree.txt > simdata.txt || exit
+
+python rel-phylip2json.py simdata.txt > simdata.json || exit
+
+set +x 
+echo 'Success!'
