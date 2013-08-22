3dd7324e960893f3b3607001924ad20236a6af48
Mark T. Holder <mtholder@gmail.com>
2013-08-22 13:08:34 -0500

    capturing seq-gen standard error stream, as we really should

--- prep/generate.sh
+++ prep/generate.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -x
 
-seq-gen -mHKY -n1000 -or tree.txt > simdata.txt || exit
+seq-gen -mHKY -n1000 -or tree.txt > simdata.txt 2>seq-gen-err-stream.txt || exit
 
 python rel-phylip2json.py simdata.txt > simdata.json || exit
 
