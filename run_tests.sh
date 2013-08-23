68be3339635154b93bbfe3a922745c0fff8edf17
Mark T. Holder <mtholder@gmail.com>
2013-08-23 00:26:11 -0500

    more testing

---
+++ run_tests.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+failures=0
+if ! sh test_shared_counts.sh
+then
+    failures=$(expr $failures + 1)
+fi
+
+if ! sh test_support_from_counts.sh
+then
+    failures=$(expr $failures + 1)
+fi
+
+if ! sh test_cutoff.sh
+then
+    failures=$(expr $failures + 1)
+fi
+
+if test $failures -gt 0
+then
+    echo 'Some tests failed (see messages above)'
+    exit 1
+fi
