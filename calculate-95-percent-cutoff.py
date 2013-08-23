2aac7e0503e58b10a0f010f032aacc1ac209d72a
Mark T. Holder <mtholder@gmail.com>
2013-08-23 01:30:33 -0500

    cutoff calculation appears to work

--- calculate-95-percent-cutoff.py
+++ calculate-95-percent-cutoff.py
@@ -61,3 +61,32 @@ import sys
 
 filename = sys.argv[1]
 inp = open(filename, 'rU')
+
+# parse the input into a list of ints
+value_list = []
+for line in inp:
+    value = int(line.strip())
+    value_list.append(value)
+
+# Sort the list, smallest to largest
+# After this the number 95% of the way through the list will be
+# the critical value
+value_list.sort()
+
+num_values = len(value_list)
+
+# this is a bit sloppy, we'll just round and then make the 
+#   index an int
+critical_index_float = 0.95*num_values
+critical_index_rounded_up = round(critical_index_float)
+critical_index = int(critical_index_rounded_up)
+
+assert critical_index > 0
+# decrement so that the index starts at 0
+critical_index -= 1
+
+# we add one because we were told to calculate a value that is
+# greater than 95 % 
+print(1 + value_list[critical_index])
+
+
