4870234cb26e5d3802297a7fe0aa21765add860f
Mark T. Holder <mtholder@gmail.com>
2013-08-23 00:17:23 -0500

    tests in place

--- calculate-95-percent-cutoff.py
+++ calculate-95-percent-cutoff.py
@@ -3,6 +3,58 @@
 Takes file with one integer per line.
 
 Writes the smallest integer that is greater than at least 95% of the numbers.
+
+For the input:
+
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+25
+
+The output should be:
+20
+
+because 20 is bigger than the first 19 out of the 20 entries
+
+Note that we'd expect the same output for 
+
+25
+0
+0
+0
+0
+0
+0
+0
+0
+0
+0
+0
+19
+0
+0
+0
+0
+0
+0
+0
+
 '''
 
 import sys
---
+++ test_cutoff.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+script=calculate-95-percent-cutoff.py
+subdir=cutoff
+
+total=0
+passed=0
+echo "testing ${script}"
+
+# run the test for all files in the input dir of the subdirectory of interest...
+for path in $(ls tests/${subdir}/input/*)
+do
+    # set some variables to make the invocation easier to read
+    #
+    filename=$(basename "${path}")
+    input="tests/${subdir}/input/${filename}"
+    output="tests/${subdir}/output/${filename}"
+    reference="tests/${subdir}/expected-output/${filename}"
+    
+    # run the script
+    #
+    echo "testing: python ${script} ${input}"
+    if python "${script}" "${input}" > "${output}"
+    then
+        # If it did not exit with an error, compare
+        # the output produced to the expected "reference" output
+        #
+        if diff "${output}" "${reference}"
+        then
+            # diff will succeed if the files are identical
+            #
+            echo "Passed"
+            passed=$(expr $passed + 1)
+        else
+            echo "Did not create the expected output!"
+        fi
+    else
+        echo "Program failed to exit cleanly"
+    fi
+    total=$(expr $total + 1)
+done
+
+echo "Passed $passed out of $total tests"
+
+if test $passed -eq $total
+then
+    exit 0
+else
+    exit 1
+fi
